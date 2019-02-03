aa532be7485414388cfd1b24909dad4b79af7a7d
MuradTcen <muradtcen@.gmail.com>
2019-02-04 04:49:11 +0500

    a

--- config/services.php
+++ config/services.php
@@ -40,4 +40,10 @@ return [
         ],
     ],
 
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => 'localhost',
+    ],
+
 ];
