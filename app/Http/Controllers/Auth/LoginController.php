4dd60ff6524f74ea3e293cb4a895ef52e6280c17
MuradTcen <muradtcen@.gmail.com>
2019-02-04 19:10:48 +0500

    a

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 use Laravel\Socialite\Facades\Socialite;
+use App\User;
 
 class LoginController extends Controller
 {
